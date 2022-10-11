cdda9a0102f5368af6aca24572c19af72780112d
JacksonSamuel <jacksonsamu42@gmail.com>
2022-10-11 15:47:14 +0100

    aula1

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-const operations = require("./modules/operations")
-const {soma, div,sub} = operations
-
-const result = soma(12, 2)
-const result2 = div(12, 4)
-const result3 = sub(12, 2)
-
-console.log(result);
-console.log(result2);
-console.log(result3);
+require("./modules/express")
